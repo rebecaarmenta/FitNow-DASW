248fe3f471d40b1b2f5950e2007c7648c34e765b
Rebeca Armenta <rebeca.armenta@iteso.mx>
2026-05-06 11:39:06 -0600

    GET info de instructores

--- BACKEND/controllers/instructors_controller.js
+++ BACKEND/controllers/instructors_controller.js
@@ -10,3 +10,18 @@ export async function getInstructors(req, res) {
         res.status(500).send(err.message);
     }
 }
+
+//GET info de instructores
+export async function getInstructorDetail(req, res) {
+    try {
+        const instructor = await User.findById(req.params.id, '-password');
+        if (!instructor || instructor.rol !== 'instructor') {
+            return res.status(404).send('Instructor no encontrado');
+        }
+        // Busca las sesiones asociadas a este instructor
+        const sessions = await Session.find({ instructor_id: instructor._id }).populate('discipline_id');
+        res.json({ instructor, sessions });
+    } catch (err) {
+        res.status(500).send(err.message);
+    }
+}
