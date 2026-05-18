853fcb862d6afaa183d27803f6c6a847a4ee241b
Rebeca Armenta <rebeca.armenta@iteso.mx>
2026-05-18 15:24:34 -0600

    clases disponibles

--- BACKEND/controllers/sessions_controller.js
+++ BACKEND/controllers/sessions_controller.js
@@ -170,7 +170,14 @@ export function deleteSession(req, res) {
 }
 
 export function getSessionsByDiscipline(req, res) {
-    Session.find({ discipline_id: req.params.discipline_id })
+    const today = new Date();
+    const startOfToday = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0);
+
+    Session.find({
+        discipline_id: req.params.discipline_id,
+        date: { $gte: startOfToday },
+        status: 'programada'
+    })
     .populate('discipline_id')
     .populate('instructor_id')
     .then(sessions => {
